0684a431e6477d90ec3cb9a1b19f97a69bc87e2b
Thiago Brezinski <thiagobrez@gmail.com>
2017-07-10 20:47:33 -0300

    Início da implementação das telas de autenticação.

--- clock/views.py
+++ clock/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def index(request):
-	pass
+	return render(request, 'index.html', {})
--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from clock import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
+	url(r'^admin/', admin.site.urls),
+	url(r'^$', views.index, name='index'),
+	url(r'^login/$', auth_views.login, name='login'),
+	url(r'^logout/$', auth_views.logout, name='logout'),
 ]
